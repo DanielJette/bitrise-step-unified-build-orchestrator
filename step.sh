5dd98276239a0e3fb84c9a85c9f571332258c4e9
Daniel Jette <daniel.jette@neofinancial.com>
2022-02-16 16:23:05 -0500

    Use curl to trigger build

--- step.sh
+++ step.sh
@@ -3,46 +3,128 @@
 NOT_CHANGED=($(./gradlew -q printModulesFromPaths -PprintAll=true))
 ALL_MODULES=( $(echo "${NOT_CHANGED[1]}" | sed 's/NOT_CHANGED=//g' | sed 's/\,/ /g') )
 
-for MODULE in "${ALL_MODULES[@]}"
-do
-  TEST_PATH=$(echo "$MODULE/src/androidTest" | sed 's/feature\-/\.\/features\//g')
-  TEST_WORKER=$(echo "test-worker-${MODULE}" | sed 's/feature\-//g')
-  if [[ -z "${FORCE_ORCHESTRATOR}" ]]; then
-    envman add --key "skip-${TEST_WORKER}" --value true
-  else
-    envman add --key "skip-${TEST_WORKER}" --value false
-  fi
-done
+function skip_unchanged_modules ()
+{
+  echo "Skip unchanged modules"
+  for MODULE in "${ALL_MODULES[@]}"
+  do
+    # TEST_PATH=$(echo "$MODULE/src/androidTest" | sed 's/feature\-/\.\/features\//g')
+    TEST_WORKER=$(echo "test-worker-${MODULE}" | sed 's/feature\-//g')
+    if [[ -z "${FORCE_ORCHESTRATOR}" ]]; then
+      echo "- Skipping $TEST_WORKER"
+      envman add --key "skip-${TEST_WORKER}" --value true
+    else
+      echo "- Testing $TEST_WORKER"
+      envman add --key "skip-${TEST_WORKER}" --value false
+    fi
+  done
+}
 
 if [[ -z "${PULL_REQUEST_ID}" ]]; then
   echo "Not a pull request, skipping"
   exit 0
 fi
 
+skip_unchanged_modules
+
 if [[ ! -z "${FORCE_ORCHESTRATOR}" ]]; then
   echo "Force orchestrator"
   exit 0
 fi
 
-PR_NUMBER=$PULL_REQUEST_ID
 
-echo "Checking $PR_NUMBER"
+function get_changed_modules()
+{
+  PR_NUMBER=$PULL_REQUEST_ID
 
-FILES=( $(curl -s -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/neofinancial/neo-android/pulls/$PR_NUMBER/files" | jq ".[$i] .filename") )
+  echo "Checking $PR_NUMBER"
 
-MODULE_PATHS=()
+  FILES=( $(curl -s -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/neofinancial/neo-android/pulls/$PR_NUMBER/files" | jq ".[$i] .filename") )
 
-for FILE in "${FILES[@]}"
-do
-  MODULE_PATH=`echo $FILE | sed -n 's/^\"\(.*\)\/src\/.*$/\1/p'`
-  MODULE_PATHS+=($MODULE_PATH)
-done
+  MODULE_PATHS=()
 
-MODULE_PATHS=($(printf "%s\n" "${MODULE_PATHS[@]}" | sort -u))
-MODULE_PATH_STRING=${MODULE_PATHS[@]}
-MODULE_PATH_STRING=${MODULE_PATH_STRING// /,}
+  for FILE in "${FILES[@]}"
+  do
+    MODULE_PATH=`echo $FILE | sed -n 's/^\"\(.*\)\/src\/.*$/\1/p'`
+    MODULE_PATHS+=($MODULE_PATH)
+  done
 
-CHANGED_MODULES=($(./gradlew -q printModulesFromPaths -Ppaths=$MODULE_PATH_STRING -PprintAll=false | sed 's/\,/ /g'))
+  MODULE_PATHS=($(printf "%s\n" "${MODULE_PATHS[@]}" | sort -u))
+  MODULE_PATH_STRING=${MODULE_PATHS[@]}
+  MODULE_PATH_STRING=${MODULE_PATH_STRING// /,}
+
+  echo ($(./gradlew -q printModulesFromPaths -Ppaths=$MODULE_PATH_STRING -PprintAll=false | sed 's/\,/ /g'))
+}
+
+function trigger_build ()
+{
+  WORKFLOW=$1
+  BRANCH=$BITRISE_GIT_BRANCH
+  BRANCH_DEST=$BITRISEIO_GIT_BRANCH_DEST
+  PULL_ID=$BITRISE_PULL_REQUEST
+  COMMIT_HASH=$BITRISE_GIT_COMMIT
+  MODULE=$2
+
+        # inputs:
+        # - workflows: test-worker-account-closure
+        # - module: feature-account-closure
+        # - variant: InternalDebugAndroidTest
+        # - target_apk: feature-account-closure-internal-debug-androidTest.apk
+        # - test_package: com.neofinancial.neo.account.closure.test
+        # - test_runner: com.neofinancial.neo.testing.AccountClosureAndroidTestRunner
+        # - environment_key_list: |-
+        #     PARENT_SLUG
+        #     TARGET_APK
+        #     ADB_COMMAND
+        #     MODULE_NAME
+        # - access_token: "$BITRISE_KEY"
+
+  echo `curl -s -X POST -H "Authorization: $BITRISE_TOKEN" "https://api.bitrise.io/v0.1/apps/60ded9f99834a343/builds" -d \
+  '{
+    "hook_info": {
+        "type":"bitrise"
+    },
+    "build_params": {
+      "workflow_id":"'$WORKFLOW'",
+      "branch":'$BRANCH',
+      "branch_dest":'$BRANCH_DEST',
+      "pull_request_id":'$PULL_ID',
+      "commit_hash":'$COMMIT_HASH',
+      "module":'$MODULE',
+      "variant":"InternalDebugAndroidTest",
+      "target_apk":'',
+      "test_package":'',
+      "test_runner":'',
+      "access_token":'$BITRISE_KEY',
+      "environments": [
+        {
+          "is_expand": true,
+          "mapped_to":"PARENT_SLUG",
+          "value":"$PARENT_SLUG"
+        },
+        {
+          "is_expand": true,
+          "mapped_to":"TARGET_APK",
+          "value":"$TARGET_APK"
+        },
+        {
+          "is_expand": true,
+          "mapped_to":"ADB_COMMAND",
+          "value":"$ADB_COMMAND"
+        },
+        {
+          "is_expand": true,
+          "mapped_to":"MODULE_NAME",
+          "value":"$MODULE_NAME"
+        }
+      ]
+    },
+    "triggered_by":"curl"
+  }'`
+}
+
+
+CHANGED_MODULES=$(get_changed_modules)
 
 for MODULE in ${CHANGED_MODULES[@]}; do
   TEST_PATH=$(echo "$MODULE/src/androidTest" | sed 's/feature\-/\.\/features\//g')
@@ -51,6 +133,9 @@ for MODULE in ${CHANGED_MODULES[@]}; do
   if [ -d "$TEST_PATH" ]; then
     echo "Building and running tests in $TEST_WORKER"
     envman add --key "skip-${TEST_WORKER}" --value false
+
+    trigger_build $TEST_WORKER $MODULE
+
   else
     echo "Changes detected, but no tests found in $MODULE"
     envman add --key "skip-${TEST_WORKER}" --value true
@@ -62,8 +147,6 @@ if [ ! -z "$TARGET_MODULE" ]; then
   echo "Force build of $TARGET_MODULE; Set $TEST_WORKER to false"
   envman add --key $TEST_WORKER --value false
 fi
-
-
 
 
 # Read all the environment variables
